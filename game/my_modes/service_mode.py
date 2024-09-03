306976407acd1094458aca706dbaaf067d7e599b
Scott Danesi <scott@scottdanesi.com>
2024-09-03 13:11:41 -0500

    Addition of ball return LEDs and a mode to switch between gameplay settings.

--- game/my_modes/service_mode.py
+++ game/my_modes/service_mode.py
@@ -9,12 +9,15 @@ import procgame.game
 import pygame
 from pygame.locals import *
 from pygame.font import *
+import logging
 
 class ServiceMode(procgame.game.Mode):
     def __init__(self, game, priority):
         super(ServiceMode, self).__init__(game=game, priority=priority)
 
-        self.menuList = ['VER','LED','SW','SOL','DSP']
+        self.logger = logging.getLogger('game.ServiceMode')
+
+        self.menuList = ['VER','LED','SW','SOL','DSP','RUL']
         self.currentMenu = 0
         self.previousMenu = 1
         self.serviceModeActive = False
@@ -33,7 +36,7 @@ class ServiceMode(procgame.game.Mode):
 
         ## Coil Test Variables #########################################################################################
         self.currentCoilIndex = 0
-        self.coilTestDelay = 1
+        self.coilTestDelay = 2
         self.coils = []
         for coil in self.game.coils:
             self.coils.append(coil)
@@ -46,30 +49,73 @@ class ServiceMode(procgame.game.Mode):
             self.add_switch_handler(name=sw.name, event_type="inactive", delay=None, handler=self.switchTestInactiveHandler)
 
     def mode_started(self):
+        # Do not activate the service mode here.  This will be handled with the enter button.
         pass
 
     def mode_stopped(self):
-        pass
+        self.deactivateServiceMode()
 
+    ## Activate and Deactivate Functions ###############################################################################
+    def activateServiceMode(self):
+        self.logger.info("###########################################")
+        self.logger.info("SERVICE MODE ENTERED")
+        self.logger.info("###########################################")
+        self.logger.debug(f"Current Menu Index: {self.currentMenu}, Current Menu Length: {len(self.menuList)}")
+        self.serviceModeActive = True
+        #will need to stop all game modes too...
+        self.game.modes.remove(self.game.attract_mode)
+
+        self.processCurrentTest()
+
+    def deactivateServiceMode(self):
+        self.logger.info("###########################################")
+        self.logger.info("SERVICE MODE EXITED")
+        self.logger.info("###########################################")
+        self.serviceModeActive = False
+        self.cancel_delayed('displayTestDelay')
+        self.cancel_delayed('coilTestDelay')
+        self.currentMenu = 0
+        self.previousMenu = 1
+        self.game.save_settings()
+        self.game.reset()
+
+    ## Change Service Menu Test Function ###############################################################################
+    def changeMode(self,direction=1):
+        self.logger.debug(f"Previous Menu Index: {self.currentMenu}, Previous Menu Length: {len(self.menuList)}")
+        self.previousMenu = self.currentMenu
+        if direction == 1:
+            # Move forward
+            if self.currentMenu < len(self.menuList) - 1:
+                self.currentMenu += 1
+        else:
+            if self.currentMenu > 0:
+                self.currentMenu -= 1
+
+        self.processCurrentTest()
+
+    ## Service Menu Processing Function ################################################################################
     def processCurrentTest(self):
-        self.game.logger.debug(f"Current Menu Index: {self.currentMenu}, Current Menu Length: {len(self.menuList)}")
+        self.logger.debug(f"Current Menu Index: {self.currentMenu}, Current Menu Length: {len(self.menuList)}")
         if self.previousMenu == self.currentMenu:
             return 0
 
         self.game.utilities_mode.disableAllLEDs("Backglass")
         self.cancel_delayed('displayTestDelay')
         self.cancel_delayed('coilTestDelay')
+        self.game.utilities_mode.disableStrobe()
         match self.menuList[self.currentMenu]:
             case 'VER':
                 ########################################################################################################
                 ## Version and Revision Display
                 ########################################################################################################
+                self.logger.info(f"SERVICE MENU VERSION ACTIVE")
                 self.game.score_display_mode.updatePlayerDisplay(1,self.game.versionMajor)
                 self.game.score_display_mode.updatePlayerDisplay(2,self.game.versionMinor)
             case 'LED':
                 ########################################################################################################
                 ## LED Test
                 ########################################################################################################
+                self.logger.info(f"SERVICE MENU LED TEST ACTIVE")
                 self.game.score_display_mode.updatePlayerDisplay(1,"LED")
                 self.game.score_display_mode.updatePlayerDisplay(2,"ALL")
                 #Start up the LED all test
@@ -77,16 +123,19 @@ class ServiceMode(procgame.game.Mode):
                     if "Backglass" in led.tags:
                         self.game.LEDs.stop_script(led.name)
                         self.game.LEDs.run_script(led.name, self.ledTestScript)
+                self.game.utilities_mode.strobeBallReturnLEDs(r=255,g=255,b=255,pulseGapS=.5,pulsetimeS=.5,strobeOverExisting=False,strobeTimeS=0)
             case 'SW':
                 ########################################################################################################
                 ## Switch Test
                 ########################################################################################################
+                self.logger.info(f"SERVICE MENU SWITCH TEST ACTIVE")
                 self.game.score_display_mode.updatePlayerDisplay(1,"S")
                 self.game.score_display_mode.updatePlayerDisplay(2,"---")
             case 'SOL':
                 ########################################################################################################
                 ## Solenoid Test
                 ########################################################################################################
+                self.logger.info(f"SERVICE MENU SOLENOID TEST ACTIVE")
                 self.game.score_display_mode.updatePlayerDisplay(1,"SOL")
                 self.game.score_display_mode.updatePlayerDisplay(2,"---")
                 # Reset Variable
@@ -97,6 +146,7 @@ class ServiceMode(procgame.game.Mode):
                 ########################################################################################################
                 ## Display Test
                 ########################################################################################################
+                self.logger.info(f"SERVICE MENU DISPLAY TEST ACTIVE")
                 # Reset display variables.
                 self.displayTestText = 0
                 self.displayTestDisplayNumber = 1
@@ -104,42 +154,17 @@ class ServiceMode(procgame.game.Mode):
 
                 # Kick off the display loop
                 self.displayTestLoop()
+            case 'RUL':
+                ########################################################################################################
+                ## Ruleset Selection
+                ########################################################################################################
+                self.game.score_display_mode.updatePlayerDisplay(1,"RUL")
+                self.game.score_display_mode.updatePlayerDisplay(2,str(self.game.user_settings['Standard']['RUL']))
 
-    def activateServiceMode(self):
-        self.game.logger.debug("###########################################")
-        self.game.logger.debug("SERVICE MODE ENTERED")
-        self.game.logger.debug("###########################################")
-        self.game.logger.debug(f"Current Menu Index: {self.currentMenu}, Current Menu Length: {len(self.menuList)}")
-        self.serviceModeActive = True
-        #will need to stop all game modes too...
-        self.game.modes.remove(self.game.attract_mode)
-
-        self.processCurrentTest()
-
-    def deactivateServiceMode(self):
-        self.serviceModeActive = False
-        self.cancel_delayed('displayTestDelay')
-        self.cancel_delayed('coilTestDelay')
-        self.currentMenu = 0
-        self.previousMenu = 1
-        self.game.reset()
-
-    def changeMode(self,direction=1):
-        self.game.logger.debug(f"Previous Menu Index: {self.currentMenu}, Previous Menu Length: {len(self.menuList)}")
-        self.previousMenu = self.currentMenu
-        if direction == 1:
-            # Move forward
-            if (self.currentMenu < len(self.menuList) - 1):
-                self.currentMenu += 1
-        else:
-            if (self.currentMenu > 0):
-                self.currentMenu -= 1
-
-        self.processCurrentTest()
-
+    ## Display Test Loop Function ######################################################################################
     def displayTestLoop(self):
         if not self.serviceModeActive:
-            self.game.logger.debug(f"Display Test Loop attempted to run when service mode was not active...")
+            self.logger.debug(f"Display Test Loop attempted to run when service mode was not active...")
             return 0
 
         self.game.utilities_mode.disableAllLEDs("NumericDisplay")
@@ -176,15 +201,16 @@ class ServiceMode(procgame.game.Mode):
 
         self.delay(name='displayTestDelay', delay=self.displayTestSpeedS, handler=self.displayTestLoop)
 
+    ## Coil Test Loop Function #########################################################################################
     def coilTestLoop(self):
         if not self.serviceModeActive:
-            self.game.logger.debug(f"Coil Test Loop attempted to run when service mode was not active...")
+            self.logger.debug(f"Coil Test Loop attempted to run when service mode was not active...")
             return 0
 
         self.game.utilities_mode.disableAllCoils()
 
         self.game.score_display_mode.updatePlayerDisplay(1,"SOL")
-        self.game.score_display_mode.updatePlayerDisplay(2,self.coils[self.currentCoilIndex].name)
+        self.game.score_display_mode.updatePlayerDisplay(2,str(self.coils[self.currentCoilIndex].label))
         self.coils[self.currentCoilIndex].pulse()
 
         if self.currentCoilIndex < len(self.coils) - 1:
@@ -194,6 +220,7 @@ class ServiceMode(procgame.game.Mode):
 
         self.delay(name='coilTestDelay', delay=self.coilTestDelay, handler=self.coilTestLoop)
 
+    ## Switch Handler Functions ########################################################################################
     def switchTestActiveHandler(self,sw):
         if self.menuList[self.currentMenu] != 'SW':
             return 0
@@ -208,10 +235,33 @@ class ServiceMode(procgame.game.Mode):
         self.game.score_display_mode.updatePlayerDisplay(1,"S-O")
         self.game.score_display_mode.updatePlayerDisplay(2,sw.number)
 
-
+    ## Switch Handlers #################################################################################################
     def sw_enter_active(self, sw):
         if not self.serviceModeActive:
             self.activateServiceMode()
+        else:
+            # Logic for changing values in Service Mode
+            match self.menuList[self.currentMenu]:
+                case 'RUL':
+                    ########################################################################################################
+                    ## Ruleset Selection
+                    ########################################################################################################
+                    valid_ruleset_values = ["A", "B"]
+                    current_ruleset = self.game.user_settings['Standard']['RUL']
+
+                    if current_ruleset in valid_ruleset_values:
+                        # Get the index of the current ruleset and move to the next one
+                        current_index = valid_ruleset_values.index(current_ruleset)
+                        next_index = (current_index + 1) % len(valid_ruleset_values)
+                        self.game.user_settings['Standard']['RUL'] = valid_ruleset_values[next_index]
+                    else:
+                        # If the current ruleset is not valid, set it to the first valid value
+                        self.game.user_settings['Standard']['RUL'] = valid_ruleset_values[0]
+
+                    # Save the game data file
+                    self.game.save_game_data()
+                    self.logger.info(f"Ruleset changed to {self.game.user_settings['Standard']['RUL']}")
+                    self.game.score_display_mode.updatePlayerDisplay(2,str(self.game.user_settings['Standard']['RUL']))
 
     def sw_up_active(self, sw):
         if self.serviceModeActive:
--- game/my_modes/utilities.py
+++ game/my_modes/utilities.py
@@ -9,10 +9,18 @@ import procgame.game
 import pygame
 from pygame.locals import *
 from pygame.font import *
+import math
+import logging
 
 class UtilitiesMode(procgame.game.Mode):
     def __init__(self, game, priority):
         super(UtilitiesMode, self).__init__(game=game, priority=priority)
+        self.logger = logging.getLogger('game.UtilitiesMode')
+
+        ## RGB Drivers Global Variables ###################################################
+        self.previousR = -1
+        self.previousG = -1
+        self.previousB = -1
 
     def mode_started(self):
         pass
@@ -40,4 +48,191 @@ class UtilitiesMode(procgame.game.Mode):
         for coil in self.game.coils:
             if tagFilter is None or tagFilter in coil.tags:
                 self.game.coils[coil.name].disable()
+
+    ####################################################################################################################
+    ## BALL RETURN LED UTILITIES
+    ####################################################################################################################
+
+    def flashBallReturnLEDs(self,flashTimeS=0.100,r=255,g=255,b=255):
+        self.setBallReturnLED(r=r,g=g,b=b)
+        self.previousR = 255
+        self.previousG = 255
+        self.previousB = 255
+        self.delay(name='speakerRGBResume',delay=flashTimeS,handler=self.resumeBallReturnLEDs)
+
+    def resumeBallReturnLEDs(self):
+        self.game.utilities_mode.update_lamps()
+
+    def disableBallReturnLEDs(self):
+        self.game.coils.BallReturnRedLED.disable()
+        self.game.coils.BallReturnGreenLED.disable()
+        self.game.coils.BallReturnBlueLED.disable()
+        self.previousR = 0
+        self.previousG = 0
+        self.previousB = 0
+        self.disableStrobe()
+        self.fade_in_progress = False
+        self.cancel_delayed('startFade')
+        self.cancel_delayed('continueFade')
+
+
+    def disableStrobe(self):
+        ## self.game.utilities_mode.disableStrobe()
+        self.strobeActive = False
+        self.cancel_delayed('strobeLoopDelay')
+
+
+    def update_lamps(self):
+        self.updateRGBBallReturn()
+
+    def updateRGBBallReturn(self):
+        self.disableStrobe()
+        self.disableBallReturnLEDs()
+
+        if self.game.attract_mode in self.game.modes:
+            self.setBallReturnLED(r=255,g=0,b=0)
+
+
+    def strobeBallReturnLEDs(self,r,g,b,pulseGapS,pulsetimeS,strobeOverExisting=False,strobeTimeS=0):
+        self.strobeActive = True
+        self.previousR = 0
+        self.previousG = 0
+        self.previousB = 0
+        self.strobeCurValR = r
+        self.strobeCurValG = g
+        self.strobeCurValB = b
+        self.strobeCurpulseGapS = pulseGapS
+        self.strobeCurpulsetimeS = pulsetimeS
+        self.strobeBallReturnLEDsLoop()
+        if strobeTimeS > 0:
+            self.delay(name='strobeDisable', delay=strobeTimeS, handler=self.updateRGBBallReturn)
+
+    def strobeBallReturnLEDsLoop(self):
+        if self.strobeActive:
+            self.setBallReturnLED(r=self.strobeCurValR, g=self.strobeCurValG, b=self.strobeCurValB)
+            self.delay(name='strobeStopDelay', delay=self.strobeCurpulsetimeS, handler=self.strobeBallReturnLEDsOff)
+            self.delay(name='strobeLoopDelay', delay=self.strobeCurpulseGapS + self.strobeCurpulsetimeS, handler=self.strobeBallReturnLEDsLoop)
+
+    def strobeBallReturnLEDsOff(self):
+        self.setBallReturnLED(r=0, g=0, b=0)
+
+    def pulseAndFadeBallReturnLEDs(self,r=255,g=0,b=255,pulse_ms=100,fade_ms=800,step_size_ms=30.0,refreshAfter=False):
+        # Stop current Fade
+        self.abortCurrentFade()
+
+        # Get variables
+        self.fade_in_progress = True
+        self.fade_total_ms = fade_ms
+        self.fade_step_size_ms = step_size_ms
+        self.fade_step_count = int(fade_ms / step_size_ms)
+        self.fade_r_reduction_per_step = int(r / self.fade_step_count)
+        self.fade_g_reduction_per_step = int(g / self.fade_step_count)
+        self.fade_b_reduction_per_step = int(b / self.fade_step_count)
+        self.current_r = r
+        self.current_g = g
+        self.current_b = b
+
+        self.disableStrobe()
+
+        #start with full value
+        self.setBallReturnLED(r=self.current_r, g=self.current_g, b=self.current_b)
+
+        if pulse_ms > 0:
+            self.delay(name='startFade', delay=pulse_ms/1000.0, handler=self.fadeLoop, param=refreshAfter)
+        else:
+            self.fadeLoop(refreshAfter=refreshAfter)
+
+    def fadeLoop(self,refreshAfter=False):
+        if self.fade_in_progress and (self.current_r > 0 or self.current_g > 0 or self.current_b > 0):
+            self.setBallReturnLED(r=self.current_r, g=self.current_g, b=self.current_b)
+            if self.current_r >= self.fade_r_reduction_per_step:
+                self.current_r = self.current_r - self.fade_r_reduction_per_step
+            else:
+                self.current_r = 0
+            if self.current_g >= self.fade_g_reduction_per_step:
+                self.current_g = self.current_g - self.fade_g_reduction_per_step
+            else:
+                self.current_g = 0
+            if self.current_b >= self.fade_b_reduction_per_step:
+                self.current_b = self.current_b - self.fade_b_reduction_per_step
+            else:
+                self.current_b = 0
+
+            self.delay(name='continueFade', delay=self.fade_step_size_ms/1000.0, handler=self.fadeLoop, param=refreshAfter)
+        else:
+            self.fade_in_progress = False
+            if refreshAfter:
+                self.updateRGBBallReturn()
+
+    def abortCurrentFade(self):
+        self.fade_in_progress = False
+        #cancel delay here
+
+    def setBallReturnLED(self,r=0,g=0,b=0,pulsetime=0):
+
+        if r == self.previousR and g == self.previousG and b == self.previousB:
+            #Do not update, just return
+            return
+
+        #Global Constants
+        self.totalResolutionMS = 10
+        self.divisor = 255 / self.totalResolutionMS
+        #Check for Reset
+        if(r==0 and g==0 and b==0):
+            self.game.coils.BallReturnRedLED.disable()
+            self.game.coils.BallReturnGreenLED.disable()
+            self.game.coils.BallReturnBlueLED.disable()
+            self.previousR = 0
+            self.previousG = 0
+            self.previousB = 0
+            return
+        #RED Color Evaluation
+        self.rOn = int(math.floor(r/self.divisor))
+        self.rOff = self.totalResolutionMS - self.rOn
+        if(self.rOn == self.totalResolutionMS):
+            if(pulsetime == 0):
+                self.game.coils.BallReturnRedLED.enable()
+            else:
+                self.game.coils.BallReturnRedLED.pulse(pulsetime)
+        elif(self.rOn == 0):
+            self.game.coils.BallReturnRedLED.disable()
+        else:
+            if(pulsetime == 0):
+                self.game.coils.BallReturnRedLED.patter(self.rOn,self.rOff,now=True)
+            else:
+                self.game.coils.BallReturnRedLED.pulsed_patter(self.rOn,self.rOff,run_time=pulsetime,now=False)
+        #GREEN Color Evaluation
+        self.gOn = int(math.floor(g/self.divisor))
+        self.gOff = self.totalResolutionMS - self.gOn
+        if(self.gOn == self.totalResolutionMS):
+            if(pulsetime == 0):
+                self.game.coils.BallReturnGreenLED.enable()
+            else:
+                self.game.coils.BallReturnGreenLED.pulse(pulsetime)
+        elif(self.gOn == 0):
+            self.game.coils.BallReturnGreenLED.disable()
+        else:
+            if(pulsetime == 0):
+                self.game.coils.BallReturnGreenLED.patter(self.gOn,self.gOff,now=True)
+            else:
+                self.game.coils.BallReturnGreenLED.pulsed_patter(self.gOn,self.gOff,run_time=pulsetime)
+        #BLUE Color Evaluation
+        self.bOn = int(math.floor(b/self.divisor))
+        self.bOff = self.totalResolutionMS - self.bOn
+        if(self.bOn == self.totalResolutionMS):
+            if(pulsetime == 0):
+                self.game.coils.BallReturnBlueLED.enable()
+            else:
+                self.game.coils.BallReturnBlueLED.pulse(pulsetime)
+        elif(self.bOn == 0):
+            self.game.coils.BallReturnBlueLED.disable()
+        else:
+            if(pulsetime == 0):
+                self.game.coils.BallReturnBlueLED.patter(self.bOn,self.bOff,now=True)
+            else:
+                self.game.coils.BallReturnBlueLED.pulsed_patter(self.bOn,self.bOff,run_time=pulsetime)
+
+        self.previousR = r
+        self.previousG = g
+        self.previousB = b
 
